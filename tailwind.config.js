7668010bb1906664a9301d7c520c279dcbb9371f
Orthadoz <Orthadoz123@gmail.com>
2024-02-01 23:50:45 +0700

    add gsap

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Prompt", "sans-serif"],
+        sans: ["IBM Plex Sans Thai Looped", "sans-serif"],
       },
     },
   },
