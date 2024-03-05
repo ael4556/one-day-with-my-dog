bb81b6216e46795039509aca5e146743b711edbe
Orthadoz <Orthadoz123@gmail.com>
2024-03-05 19:52:47 +0700

    fixed cardflip.js to click to flip

--- src/components/cardflip.js
+++ src/components/cardflip.js
@@ -1,10 +1,24 @@
 import image_frame from "../Images/story_8/command-cards-1.png";
+import { useState } from "react";
 
 function CardFlip({ image_front = image_frame, image_back = image_frame }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
     <div className="flex flex-col justify-center ">
-      <div className="group h-[448px] w-[308px] [perspective:1000px]">
-        <div className=" relative h-full w-full transition-all duration-700 rounded-full shadow-xl [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+      <div
+        onClick={handleFlip}
+        className="group h-[448px] w-[308px] [perspective:1000px]"
+      >
+        <div
+          className={`relative h-full w-full transition-all duration-700 rounded-full shadow-xl [transform-style:preserve-3d] 
+         ${isFlipped ? "[transform:rotateY(180deg)]" : ""}
+        `}
+        >
           <div className=" absolute inset-0">
             <img
               className="h-full w-full object-cover shadow-xl"
