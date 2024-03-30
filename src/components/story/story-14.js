9cf9cfabca4507d88f6e7339ffe426590b80f0cc
ael4556 <aelkung101@gmail.com>
2024-03-31 02:18:08 +0700

    add animation for st-14

--- src/components/story/story-14.js
+++ src/components/story/story-14.js
@@ -3,7 +3,7 @@ import Dog from "../../Images/story_14/dog-st-14.png";
 import Bin from "../../Images/story_14/bin.png";
 import Poop from "../../Images/story_14/poop-st-14.png";
 import Hand from "../../Images/story_14/hand-with-bag.gif";
-import VFX from "../../Images/story_14/vfx.gif";
+import Vfx from "../../Images/story_14/vfx.gif";
 import BGTextBot from "../../Images/story_14/text-bg-1.png";
 import BGTextTop from "../../Images/story_14/text-bg-2.png";
 import HowTo from "../../Images/story_14/Pickupshit.gif";
@@ -16,25 +16,186 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 function Story14() {
-    
+    const text_story14_1Ref = useRef ();
+    const text_story14_2Ref = useRef ();
+    const title_EmergencyRef = useRef ();
+    const text_story14_3Ref = useRef ();
+    const text_story14_4Ref = useRef ();
+    const text_story14_5Ref = useRef ();
+    const bg_howtoRef = useRef ();
+    const bg_howto2Ref = useRef ();
+
     useEffect(() => {
-      
+    const text_story14_1 = text_story14_1Ref.current;
+    const text_story14_2 = text_story14_2Ref.current;
+    const title_Emergency = title_EmergencyRef.current;
+    const text_story14_3 = text_story14_3Ref.current;
+    const text_story14_4 = text_story14_4Ref.current;
+    const text_story14_5 = text_story14_5Ref.current;
+    const bg_howto = bg_howtoRef.current;
+    const bg_howto2 = bg_howto2Ref.current;
+
+    gsap.fromTo(
+        text_story14_1,
+        { opacity: 0, scale: 0.9 },
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 1,
+          delay: 0,
+          scrollTrigger: {
+            trigger: text_story14_1,
+            scrub: true,
+            start: "0px 80%",
+            end: "100px 50%",
+  
+            /*markers : true,*/
+          },
+        }
+      );
+
+      gsap.fromTo(
+        text_story14_2,
+        { opacity: 0, scale: 0.9 },
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 1,
+          delay: 0,
+          scrollTrigger: {
+            trigger: text_story14_2,
+            scrub: true,
+            start: "0px 80%",
+            end: "100px 50%",
+  
+            /*markers : true,*/
+          },
+        }
+      );
+
+      gsap.fromTo(
+        title_Emergency,
+        { scale: 0 },
+        {
+          scale: 1,
+          duration: 1,
+          scrollTrigger: {
+            trigger: title_Emergency,
+            scrub: true,
+            start: "0px 50%",
+            end: "100px 20%",
+  
+            /*markers : true,*/
+          },
+        }
+      );
+
+      gsap.fromTo(
+        text_story14_3,
+        { opacity: 0, scale: 0.9 },
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 1,
+          delay: 0,
+          scrollTrigger: {
+            trigger: text_story14_3,
+            scrub: true,
+            start: "0px 80%",
+            end: "100px 50%",
+  
+            /*markers : true,*/
+          },
+        }
+      );
+
+      gsap.fromTo(
+        text_story14_4,
+        { opacity: 0, scale: 0.9 },
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 1,
+          delay: 0,
+          scrollTrigger: {
+            trigger: text_story14_4,
+            scrub: true,
+            start: "0px 80%",
+            end: "100px 50%",
+
+            /*markers : true,*/
+          },
+        }
+      );
+
+      gsap.fromTo(
+        text_story14_5,
+        { opacity: 0, scale: 0.8 },
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 1,
+          delay: 0,
+          scrollTrigger: {
+            trigger: text_story14_5,
+            scrub: true,
+            start: "300px 70%",
+            end: "200px 40%",
+
+            /*markers : true,*/
+          },
+        }
+      );
+
+      gsap.fromTo(
+        bg_howto,
+        { xPercent : -20 },
+        {
+            xPercent: 0,
+            duration: 1,
+            scrollTrigger: {
+            trigger: bg_howto,
+            scrub: true,
+            start: "0px 100%",
+            end: "100px 50%",
+
+            /*markers : true,*/
+          },
+        }
+      );
+
+      gsap.fromTo(
+        bg_howto2,
+        { xPercent : 15, },
+        {
+            xPercent: 0,
+            duration: 1,
+            scrollTrigger: {
+            trigger: bg_howto2,
+            scrub: true,
+            start: "0px 100%",
+            end: "100px 50%",
+
+            /*markers : true,*/
+          },
+        }
+      );
 
     }, []);
     return (
         <div className=" pt-[5%] flex flex-col">
             <div className="flex flex-col">
-                <div className="flex justify-center">
+                <div className="flex justify-center" ref={title_EmergencyRef}>
                     <img className="w-[60%] absolute z-0" src={Emergency}/>
                 </div>
                 <div className=" grid grid-cols-3 pt-[30%]">
                     <div></div>
                     <div className=" font-sans text-3xl z-10">
-                        <p className="flex justify-center leading-relaxed text-center">
+                        <p className="flex justify-center leading-relaxed text-center" ref={text_story14_1Ref}>
                             มีเหตุการณ์ไม่คาดคิด มีตังค์ดันทำท่าเหมือนจะอึ 
                             คุณรีบหันไปด้วยความตกใจ พร้อมกับทำตาโต
                         </p>
-                        <p className="flex justify-center leading-relaxed text-center pt-[15%]">
+                        <p className="flex justify-center leading-relaxed text-center pt-[15%]" ref={text_story14_2Ref}>
                             ...และใช่ คุณไม่สามารถห้ามเหตุการณ์นี้ได้ทัน
                             และต้องรับกรรมไป
                         </p>
@@ -67,24 +228,24 @@ function Story14() {
                 </div>
             </div>
             <div className="pt-[10%]">
-                <div className=" absolute left-[10%] pt-[27%]">
+                <div className=" absolute left-[10%] pt-[27%]" ref={text_story14_5Ref}>
                     <p className="font-sans text-8xl text-[#4091AE] font-semibold z-0">ถุงขยะเก็บอึอึ๊น้องหมา!</p>
                 </div>
                 <div className=" grid grid-cols-2 z-20">
                     <div className="pl-[20%]">
-                        <div className=" font-sans text-3xl pt-[20%]">
+                        <div className=" font-sans text-3xl pt-[20%]" ref={text_story14_3Ref}>
                             <p>บ่นไปก็เท่านั้น..ก็ต้องเก็บให้</p>
                             <p className="pt-[1.5%]">เจ้าตัวสร้างเรื่องอยู่ดี</p>
                         </div>
-                        <div className=" font-sans text-6xl pt-[20%]">
+                        <div className=" font-sans text-6xl pt-[20%]" ref={text_story14_4Ref}>
                             <p>ดีนะที่ม่อน... เอ้ย!! เรามี</p>
                         </div>                   
                     </div>
                     <div>
-                        <div className="flex justify-end pr-[5%]">
-                            <img className="w-[70%] z-10" src={Hand}/>
+                        <div className="flex justify-end pr-[5%]" >
+                            <img className="w-[70%] z-10" src={Hand} />
                             <div className="absolute w-[37%] pr-[7%]">
-                            <img className="z-0" src={VFX}/>
+                            <img className="z-0" src={Vfx}/>
                             </div>
                         </div>
                     </div>        
@@ -92,20 +253,20 @@ function Story14() {
             </div>
             <div className="pt-[10%]">
                 <div className=" flex justify-center">
-                    <div className=" absolute pt-[7%]">
+                    <div className=" absolute pt-[7%]" >
                         <p className=" font-sans font-medium text-6xl text-[#4091AE]">จะกี่ก้อนก็ไม่หวั่น แม้วันมามาก</p>
                     </div>
                 </div>
                 <div className="flex justify-center">
-                    <div className=" absolute pt-[15%]">
+                    <div className=" absolute pt-[15%] z-50">
                         <img className="" src={HowTo}/>
                     </div>
                 </div>
-                <div className="flex justify-start">
-                    <img className="" src={BGTextTop}/>
+                <div className="flex justify-start" ref={bg_howtoRef}>
+                    <img className=" z-0" src={BGTextTop}/>
                 </div>
-                <div className="flex justify-end pt-[5%]">
-                    <img className="" src={BGTextBot}/>
+                <div className="flex justify-end pt-[5%]" ref={bg_howto2Ref}>
+                    <img className=" z-0" src={BGTextBot}/>
                 </div>
             </div>
             
