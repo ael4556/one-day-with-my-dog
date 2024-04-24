9f15df25fe39ab9ab0e81e9015e37e6257193a6d
ael4556 <aelkung101@gmail.com>
2024-04-25 02:48:23 +0700

    edit welcome

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -24,17 +24,17 @@ function Welcome() {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      <div className="bg-white p-8 rounded-[20px] shadow-lg max-w-lg">
+      <div className="bg-white p-[1.673vw] rounded-[20px] shadow-lg max-w-lg">
                 <div className=" flex justify-end">
                     <img
                     onClick={handleClose}
                     src={close}
-                    className="w-[7%] cursor-pointer hover:bg-gray-200 rounded-full hover:scale-105"
+                    className="w-[7vw] cursor-pointer hover:bg-gray-200 rounded-full hover:scale-105"
                     alt="close"
                     />
                 </div>
-        <h2 className="text-3xl font-Prompt font-semibold mb-4 tracking-wide">ยินดีต้อนรับ !</h2>
-        <p className="text-gray-700 font-Prompt text-2xl leading-relaxed">เข้าสู่โหมดเต็มหน้าจอ เพื่อรับประสบการณ์ที่ดีที่สุดในการใช้งานเว็บไซต์ โดยกด F11 (สำหรับ Window) และ fn+f (สำหรับ MacOS)</p>
+        <h2 className="text-[1.8vw] font-Prompt font-semibold mb-[0.83vw] tracking-wide">ยินดีต้อนรับ !</h2>
+        <p className="text-gray-700 font-Prompt text-[1.67vw] leading-relaxed">เข้าสู่โหมดเต็มหน้าจอ เพื่อรับประสบการณ์ที่ดีที่สุดในการใช้งานเว็บไซต์ โดยกด F11 (สำหรับ Window) และ fn+f (สำหรับ MacOS)</p>
       </div>
     </div>
   );
